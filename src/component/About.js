1bc76657c40c3eee20dfc1bb8216bbf77a20d251
Varalaxmi <varalaxmi@example.com>
2025-02-06 21:30:57 +0530

    new changes

--- src/component/About.js
+++ src/component/About.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
+import "../App.css"
 
 const About = () => {
   return (
--- src/component/Teachings.js
+++ src/component/Teachings.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
+import "../App.css"
 
 const Teachings = () => {
   // Data for teachings and their details
